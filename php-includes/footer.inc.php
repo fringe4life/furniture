d54736a5c175eb2f6582893f0ce41ed97df0f69b
fringe4life <coinnich09@gmail.com>
2015-03-22 19:34:54 +1300

    ajax
    made use of ajax to load the other tabs

--- php-includes/footer.inc.php
+++ php-includes/footer.inc.php
@@ -1,7 +1,1 @@
-        <footer class="center">
-            <p >Copyright &copy; Chris Wilson 2004</p>
-            <p ><span id="email">@</span><a href="mailto:chris@chriswilsondesign.co.nz">chris@chriswilsondesign.co.nz</a></p>
-        </footer>
-    </body>
-
-</html>
+<footer class="center"><p>Copyright &copy; Chris Wilson 2004</p><p><span id="email">@</span><a href="mailto:chris@chriswilsondesign.co.nz">chris@chriswilsondesign.co.nz</a></p></footer></body></html>
